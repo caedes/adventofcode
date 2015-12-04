6855bcf4b838e78c50217b9c9de5d7e5a52f4c61
caedes <laurentromain@gmail.com>
2015-12-05 00:23:55 +0100

    Add day2

---
+++ day2.rb
@@ -0,0 +1,14 @@
+areas = File.readlines('inputs/day2.txt').map do |line|
+  dimensions = line.split('x').map(&:to_i)
+  l = dimensions[0]
+  w = dimensions[1]
+  h = dimensions[2]
+
+  lw = l * w
+  wh = w * h
+  hl = h * l
+
+  2 * lw + 2 * wh + 2 * hl + [lw, wh, hl].min
+end
+
+p areas.inject { |sum, x| sum + x }
